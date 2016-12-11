0b9b9f9108e8eb8f87d8f1f35c2319e0baa33311
fer-dev <the.crazy.shadows@outlook.es>
2016-12-11 12:54:05 -0500

    Update gclone.sh

--- gclone.sh
+++ gclone.sh
@@ -4,7 +4,7 @@
 #choose the list of the files you want to clone:
   echo "insert the file"
 
-  read /repoclone/list
+  read /repoclone/list.txt
 
 for i in $( cat list.txt );do
         echo  "--------------------------------------------------------------------"
